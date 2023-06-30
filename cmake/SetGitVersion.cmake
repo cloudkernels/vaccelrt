86cdba04f4466cf6544d5650bdbb1162ce498965
Kostis Papazafeiropoulos <papazof@gmail.com>
2023-06-30 18:51:01 +0300

    Generate and install cmake config/version files
    - Generate and install vaccel-config.cmake and vaccel-config-version.cmake
    - Base project version on git version
    
    Signed-off-by: Kostis Papazafeiropoulos <papazof@gmail.com>

--- cmake/SetGitVersion.cmake
+++ cmake/SetGitVersion.cmake
@@ -1,16 +1,20 @@
+find_package(Git)
+
 if(NOT DEFINED VACCELRT_VERSION)
 	if(GIT_EXECUTABLE)
-		get_filename_component(SRC_DIR ${SRC} DIRECTORY)
 		# Generate a git-describe version string from Git repository tags
 		execute_process(
 			COMMAND ${GIT_EXECUTABLE} describe --tags --dirty --match "v*" --always
-			WORKING_DIRECTORY ${SRC_DIR}
+			WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}/src"
 			OUTPUT_VARIABLE GIT_DESCRIBE_VERSION
 			RESULT_VARIABLE GIT_DESCRIBE_ERROR_CODE
 			OUTPUT_STRIP_TRAILING_WHITESPACE
 			)
 		if(NOT GIT_DESCRIBE_ERROR_CODE)
 			set(VACCELRT_VERSION ${GIT_DESCRIBE_VERSION})
+		else()
+			set(VACCELRT_VERSION v0.0.0-unknown)
+			message(WARNING "Failed to determine VACCELRT_VERSION from Git tags. Using default version \"${VACCELRT_VERSION}\".")
 		endif()
 	else()
 		# Final fallback: Just use a bogus version string that is semantically older
@@ -20,4 +24,4 @@ if(NOT DEFINED VACCELRT_VERSION)
 	endif()
 endif()
 
-configure_file(${SRC} ${DST} @ONLY)
+string(REGEX REPLACE "v([^-]+)-.*" "\\1" VACCELRT_VERSION_BASE "${VACCELRT_VERSION}")
